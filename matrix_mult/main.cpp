381d7370caeb7d3459ffd37f39d6baf84e9429e2
Jimmy Zhang <jiemingjimmy@gmail.com>
2021-11-08 13:59:33 -0600

    arrays in heap

--- matrix_mult/main.cpp
+++ matrix_mult/main.cpp
@@ -2,22 +2,25 @@
 #include <cstdint>
 #include <stdlib.h> 
 #include <unistd.h>
-
+#include <vector>
 
 using namespace std;
 
 #define MOD 7919
+#define m 1000
+#define n 1000
+#define k 1000
 
 class LCG{
     private:
         uint32_t curr = 0;
         uint32_t prev = 0;
-        uint32_t a,m,c;
+        uint32_t a,mod,c;
     public:
-        LCG(uint32_t a, uint32_t b, uint32_t m){
+        LCG(uint32_t a, uint32_t b, uint32_t mod){
             this->a = a;
             this->c = c;
-            this->m = m;
+            this->mod = mod;
         }
         uint32_t generate(){
             prev = curr;
@@ -28,26 +31,14 @@ class LCG{
 
 
 int main(int argc, char* argv[]){
-    int mode=0;
-    int m=10;
-    int n=10;
-    int k=10;
+    int version=0;
     bool print = false;
 
     int option;
     while((option = getopt(argc, argv, "m:n:k:m:p:v:")) != -1){ //get option from the getopt() method
       switch(option){
-         case 'm':
-            m = atoi(optarg);
-            break;
-         case 'n': 
-            n = atoi(optarg);
-            break;
-         case 'k':
-            k = atoi(optarg);
-            break;
          case  'v':
-            mode = atoi(optarg);
+            version = atoi(optarg);
             break;
          case 'p': 
             print = atoi(optarg)==1?1:0;
@@ -55,47 +46,98 @@ int main(int argc, char* argv[]){
       }
    }
 
-    cout<<"mode="<<mode<<", m="<<m<<", n="<<n<<", k="<<k<<'\n';
+    cout<<"mode="<<version<<", m="<<m<<", n="<<n<<", k="<<k<<'\n';
     
     LCG lcg = LCG(55, 91, MOD);
-    uint32_t a[m][k];
+    vector<vector<uint32_t>> a(
+        m, vector<uint32_t>(k,0));
     for (uint32_t i=0; i<m; i++){
         for (uint32_t j=0; j<k; j++)
             a[i][j] = lcg.generate();
         
     }
-    uint32_t b[n][k];
+    vector<vector<uint32_t>> b(
+        n, vector<uint32_t>(k,0));
     for (uint32_t i=0; i<n; i++){
         for (uint32_t j=0; j<k; j++)
             b[i][j] = lcg.generate();
         
     }
-    uint32_t c[m][n] = {0};
-    for (uint32_t i=0; i<m; i++){
-        for (uint32_t j=0; j<n; j++)
-            c[i][j] = 0;
-    }
-    if (mode==0){
+    vector<vector<uint32_t>> c(
+        m, vector<uint32_t>(n,0));
+
+    if (version==0){
         cout<<"Entering matrix mult loops...\n";
+
+        #pragma unroll
         for (uint32_t i=0; i<m; i++){
+            #pragma unroll
             for (uint32_t j=0; j<n; j++){
+                #pragma unroll
                 for (uint32_t l=0; l<k; l++){
                     c[i][j] += a[i][l]*b[l][j];
                 }
             }
         }
     }
-    else if (mode==1){
+    else if (version==1){
         cout<<"Entering matrix mult loops...\n";
-        
+        #pragma unroll
         for (uint32_t l=0; l<k; l++){
+            #pragma unroll
             for (uint32_t i=0; i<m; i++){
+                #pragma unroll
+                for (uint32_t j=0; j<n; j++){
+                    c[i][j] += a[i][l]*b[l][j];
+                }
+            }
+        }      
+    } 
+    else if (version==2){
+        cout<<"Entering matrix mult loops...\n";
+        #pragma unroll
+        for (uint32_t i=0; i<m; i++){
+            #pragma unroll
+            for (uint32_t l=0; l<k; l++){
+                #pragma unroll
                 for (uint32_t j=0; j<n; j++){
                     c[i][j] += a[i][l]*b[l][j];
                 }
             }
         }      
     }  
+    else if (version==5){
+        cout<<"Entering matrix mult loops...\n";
+        #pragma unroll
+        for (uint32_t i=0; i<m; i++){
+            #pragma unroll
+            for (uint32_t l=0; l<k; l++){
+                #pragma unroll
+                for (uint32_t j=0; j<n; j++){
+                    c[i][j] += a[i][l]*b[l][j];
+                }
+            }
+        }      
+    } 
+    else if (version==3){
+        #define BLOCK 10
+
+        #pragma unroll
+        for (uint32_t i0=0; i0<m; i0=i0+BLOCK){
+            #pragma unroll
+            for (uint32_t l0=0; l0<k; l0=l0+BLOCK){
+                #pragma unroll
+                for (uint32_t j0=0; j0<n; j0=j0+BLOCK){
+
+                    #pragma unroll
+                    for (uint32_t i=0; i<BLOCK; ++i){
+                        #pragma unroll
+                        for (uint32_t l=0; l<BLOCK; ++l){
+                            #pragma unroll
+                            for (uint32_t j=0; j<BLOCK; ++j){
+                                c[i][j] += a[i][l]*b[l][j];
+        }}}}}}  
+    }
     else{
 
     }
