ad4c83ab1fbc7f300bcdc590ede6510514655152
Jimmy Zhang <jiemingjimmy@gmail.com>
2022-04-04 19:39:51 -0500

    updated binarys

--- build.sh
+++ build.sh
@@ -29,23 +29,23 @@ make clean && make
 mv hw ../tests
 cd ..
 
-# echo "*****MATRIX MULT*****"
-# rm tests/matrix_mult && rm tests/matrix_mult.fast
-# cd matmul 
-# make clean && make
-# cp matrix_mult ../tests
-# echo "*****MATRIX MULT O3*****"
-# make clean
-# make fast
-# mv matrix_mult.fast ../tests
-# cd ..
+echo "*****MATRIX MULT*****"
+rm tests/matrix_mult && rm tests/matrix_mult.fast
+cd matmul 
+make clean && make
+cp matrix_mult ../tests
+echo "*****MATRIX MULT O3*****"
+make clean
+make fast
+mv matrix_mult.fast ../tests
+cd ..
 
 
 
-# echo "*****DFS GRAPH*****"
-# # rm tests/graphs_dfs
-# cd graph_dfs
-# make fast
-# mv graph_dfs.fast ../tests
-# cd ..
+echo "*****DFS GRAPH*****"
+# rm tests/graphs_dfs
+cd graph_dfs
+make fast
+mv graph_dfs.fast ../tests
+cd ..
 
